0e5f7d30ba2b65d49e1d9dced90969e3baee4a4c
MSChoi <MSChoi@stu.kmu.ac.kr>
2018-05-15 16:12:17 +0900

    Acceptance

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-test $(curl localhost:8765/sum?a=1\&b=2) -eq 3
+test $(curl $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' calculator):8090/sum?a=1\&b=2) -eq 3
